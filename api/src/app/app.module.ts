154c18621cb58d7dde6fae77575ab77211588808
Matheus Fiebig <macrubig@gmail.com>
2024-09-13 20:01:29 -0300

    chore: limpeza de template

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
@@ -17,12 +15,9 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   imports: [
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule,
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
-    NgbModule,
-    ToastrModule.forRoot()
   ],
   declarations: [
     AppComponent,
