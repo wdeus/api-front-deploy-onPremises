67d29663cd8136d3064c507e74aaa25db2c71a80
beatricelopes <beatricelopes@outlook.com>
2024-09-12 21:22:54 -0300

    feat: site template

--- api/src/app/app.component.ts
+++ api/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api';
+
 }
