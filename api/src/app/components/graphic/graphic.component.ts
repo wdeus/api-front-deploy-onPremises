17d492351daf033659339659d6629b596249c933
Matheus Fiebig <macrubig@gmail.com>
2024-09-16 22:39:59 -0300

    feat: finalizado componente generico

--- api/src/app/components/graphic/graphic.component.ts
+++ api/src/app/components/graphic/graphic.component.ts
@@ -1,160 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-graphic',
-  templateUrl: './graphic.component.html',
-  styleUrls: ['./graphic.component.scss']
+    selector: 'app-graphic',
+    templateUrl: './graphic.component.html',
+    styleUrls: ['./graphic.component.scss']
 })
 export class GraphicComponent implements OnInit {
-  public gradientStroke: any;
-  public chartColor: any;
-  public gradientFill: any;
-  public gradientChartOptionsConfiguration: any;
-  public gradientChartOptionsConfigurationWithNumbersAndGrid: any;
+    public gradientStroke: any;
+    public chartColor = "#FFFFFF";
+    public canvas: any;
+    public ctx: any;
+    public gradientFill: any;
+    public gradientChartOptionsConfiguration: any;
+    public gradientChartOptionsConfigurationWithNumbersAndGrid: any;
 
-  public lineChartType: any;
-  public lineChartData: Array<any>;
-  public lineChartOptions: any;
-  public lineChartLabels: Array<any>;
-  public lineChartColors: Array<any>
+    @Input() public chartId: string = 'lineChartExample';
+    @Input() public lineChartType: string = 'line';
+    @Input() public lineChartData: Array<any>;
+    @Input() public lineChartOptions: any;
+    @Input() public lineChartLabels: Array<any>;
+    @Input() public lineChartColors: Array<any>
 
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-    this.gradientChartOptionsConfiguration = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      tooltips: {
-        bodySpacing: 4,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest",
-        xPadding: 10,
-        yPadding: 10,
-        caretPadding: 10
-      },
-      responsive: 1,
-      scales: {
-        yAxes: [{
-          display: 0,
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawTicks: false,
-            display: false,
-            drawBorder: false
-          }
-        }],
-        xAxes: [{
-          display: 0,
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawTicks: false,
-            display: false,
-            drawBorder: false
-          }
-        }]
-      },
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 15,
-          bottom: 15
+    ngOnInit(): void {
+        this.canvas = document.getElementById(this.chartId);
+        this.ctx = this.canvas.getContext("2d");
+
+        this.gradientChartOptionsConfiguration = {
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            tooltips: {
+                bodySpacing: 4,
+                mode: "nearest",
+                intersect: 0,
+                position: "nearest",
+                xPadding: 10,
+                yPadding: 10,
+                caretPadding: 10
+            },
+            responsive: 1,
+            scales: {
+                yAxes: [{
+                    display: 0,
+                    ticks: {
+                        display: false
+                    },
+                    gridLines: {
+                        zeroLineColor: "transparent",
+                        drawTicks: false,
+                        display: false,
+                        drawBorder: false
+                    }
+                }],
+                xAxes: [{
+                    display: 0,
+                    ticks: {
+                        display: false
+                    },
+                    gridLines: {
+                        zeroLineColor: "transparent",
+                        drawTicks: false,
+                        display: false,
+                        drawBorder: false
+                    }
+                }]
+            },
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 15,
+                    bottom: 15
+                }
+            }
+        };
+
+        this.gradientFill = this.ctx.createLinearGradient(0, 170, 0, 50);
+        this.gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+        this.gradientFill.addColorStop(1, "rgba(249, 99, 59, 0.40)");
+
+        this.lineChartData = [
+            {
+                label: "Active Users",
+                pointBorderWidth: 2,
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 1,
+                pointRadius: 4,
+                fill: true,
+                borderWidth: 2,
+                data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]
+            }
+        ];
+        this.lineChartColors = [
+            {
+                borderColor: "#f96332",
+                pointBorderColor: "#FFF",
+                pointBackgroundColor: "#f96332",
+                backgroundColor: this.gradientFill
+            }
+        ];
+        this.lineChartLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        this.lineChartOptions = this.gradientChartOptionsConfiguration;
+    }
+
+    public hexToRGB(hex, alpha) {
+        var r = parseInt(hex.slice(1, 3), 16),
+            g = parseInt(hex.slice(3, 5), 16),
+            b = parseInt(hex.slice(5, 7), 16);
+
+        if (alpha) {
+            return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+        } else {
+            return "rgb(" + r + ", " + g + ", " + b + ")";
         }
-      }
-    };
+    }
 
-    this.gradientChartOptionsConfigurationWithNumbersAndGrid = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      tooltips: {
-        bodySpacing: 4,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest",
-        xPadding: 10,
-        yPadding: 10,
-        caretPadding: 10
-      },
-      responsive: true,
-      scales: {
-        yAxes: [{
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawBorder: false
-          },
-          ticks: {
-            stepSize: 500
-          }
-        }],
-        xAxes: [{
-          display: 0,
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawTicks: false,
-            display: false,
-            drawBorder: false
-          }
-        }]
-      },
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 15,
-          bottom: 15
-        }
-      }
-    };
-
-    const canvas = document.getElementById("lineChartExample") as any;
-    const ctx = canvas.getContext("2d");
-
-    this.gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
-    this.gradientStroke.addColorStop(0, '#80b6f4');
-    this.gradientStroke.addColorStop(1, this.chartColor);
-
-    this.gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
-    this.gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
-    this.gradientFill.addColorStop(1, "rgba(249, 99, 59, 0.40)");
-
-    this.lineChartData = [
-      {
-        label: "Active Users",
-        pointBorderWidth: 2,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 1,
-        pointRadius: 4,
-        fill: true,
-        borderWidth: 2,
-        data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]
-      }
-    ];
-    this.lineChartColors = [
-      {
-        borderColor: "#f96332",
-        pointBorderColor: "#FFF",
-        pointBackgroundColor: "#f96332",
-        backgroundColor: this.gradientFill
-      }
-    ];
-    this.lineChartLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    this.lineChartOptions = this.gradientChartOptionsConfiguration;
-    this.lineChartType = 'line';
-  }
 
 }
