f27b3ea9e1d63d3ceb32e7a0fd44f7407f8e7de0
beatricelopes <beatricelopes@outlook.com>
2024-09-22 16:13:00 -0300

    PX-16 - Testes Unitarios

--- api/src/app/dashboard/dashboard.component.spec.ts
+++ api/src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { CardData, DashboardService, GraphicData } from '../services/dashboard.service';
+import { of } from 'rxjs';
+
+class MockCanvas {
+  getContext() {
+      return new MockContext();
+  }
+}
+
+class MockContext {
+  createLinearGradient() {
+      return new MockGradient();
+  }
+}
+
+class MockGradient {
+  colorStops = [];
+  constructor() {
+      this.colorStops = [];
+  }
+
+  addColorStop(position, color) {
+      this.colorStops.push({ position, color });
+  }
+}
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let serviceSpy: jasmine.SpyObj<DashboardService>
 
   beforeEach(async(() => {
+    serviceSpy = jasmine.createSpyObj<DashboardService>('DashboardService', ['getCardData', 'getGraphicData'])
+
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        {
+          provide: DashboardService,
+          useValue: serviceSpy
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +51,59 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+    spyOn(document, 'getElementById').and.callFake((id: string) => {
+      if (id === "mainChart") {
+          return new MockCanvas() as any;
+      }
+      return null;
+  });
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    spyOn(component, 'loadData').and.callFake(()=>{})
     expect(component).toBeTruthy();
   });
+
+  it('should load card and graphic data and set parameters correctly', () => {
+    const requestCardOne = component.createCardRequest(1);
+    const requestCardTwo = component.createCardRequest(2);
+    const requestCardThree = component.createCardRequest(3);
+    
+    const requestGraphicOne = component.createGraphicRequest(1);
+    const requestGraphicTwo = component.createGraphicRequest(2);
+    const requestGraphicThree = component.createGraphicRequest(3);
+  
+    const cardOneData  = [10] as CardData;
+    const cardTwoData = [20] as CardData;
+    const cardThreeData = [30] as CardData;
+  
+    const graphicOneData = [[5, 'Label1'] as GraphicData, [10, 'Label2'] as GraphicData];
+    const graphicTwoData = [[15, 'Label3'] as GraphicData,  [20, 'Label4'] as GraphicData] ;
+    const graphicThreeData = [[25, 'Label5'] as GraphicData, [30, 'Label6'] as GraphicData];
+  
+    serviceSpy.getCardData.and.returnValues(
+      of(cardOneData),
+      of(cardTwoData),
+      of(cardThreeData)
+    );
+  
+    serviceSpy.getGraphicData.and.returnValues(
+      of(graphicOneData), 
+      of(graphicTwoData), 
+      of(graphicThreeData)
+    );
+  
+    component.loadData();
+  
+    expect(component.cardData).toEqual([
+      { value: 10, request: requestCardOne },
+      { value: 20, request: requestCardTwo },
+      { value: 30, request: requestCardThree }
+    ]);
+  
+
+    expect(component.isLoading).toBeFalse();
+  });
+  
 });
--- api/src/app/services/dashboard.service.spec.ts
+++ api/src/app/services/dashboard.service.spec.ts
@@ -1,16 +1,108 @@
 import { TestBed } from '@angular/core/testing';
-
-import { DashboardService } from './dashboard.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { DashboardService, CardData, GraphicData } from './dashboard.service';
+import { DashboardRequest } from '../models/dashboard-request.model';
+import { environment } from '../../environments/environment';
 
 describe('DashboardService', () => {
   let service: DashboardService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DashboardService]
+    });
     service = TestBed.inject(DashboardService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verifica se não há requisições pendentes
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#getCardData', () => {
+    it('should return CardData on success', () => {
+      const mockRequest: DashboardRequest = {
+        description: 'Gráfico de vendas por região',
+        eixoX: {
+          nome: 'Região',
+          campo: 'region'
+        },
+        eixoY: {
+          nome: 'Vendas',
+          campo: 'sales'
+        },
+        filtros: [
+          {
+            nome: 'Ano',
+            campo: 'year',
+            comparador: '>=',
+            valor: '2020'
+          },
+          {
+            nome: 'Região',
+            campo: 'region',
+            comparador: '=',
+            valor: 'Norte'
+          }
+        ]
+      };;
+      const mockResponse: CardData = [42];
+
+      service.getCardData(mockRequest).subscribe((data) => {
+        expect(data).toEqual(mockResponse);
+      });
+
+      const req = httpMock.expectOne(`${environment.apiUrl}visualizacao/card`);
+      expect(req.request.method).toBe('POST');
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('#getGraphicData', () => {
+    it('should return GraphicData array on success', () => {
+      const mockRequest: DashboardRequest = {
+        description: 'Gráfico de vendas por região',
+        eixoX: {
+          nome: 'Região',
+          campo: 'region'
+        },
+        eixoY: {
+          nome: 'Vendas',
+          campo: 'sales'
+        },
+        filtros: [
+          {
+            nome: 'Ano',
+            campo: 'year',
+            comparador: '>=',
+            valor: '2020'
+          },
+          {
+            nome: 'Região',
+            campo: 'region',
+            comparador: '=',
+            valor: 'Norte'
+          }
+        ]
+      };;
+      const mockResponse: GraphicData[] = [
+        [10, 'Label1'],
+        [20, 'Label2']
+      ];
+
+      service.getGraphicData(mockRequest).subscribe((data) => {
+        expect(data).toEqual(mockResponse);
+      });
+
+      const req = httpMock.expectOne(`${environment.apiUrl}visualizacao/grafico`);
+      expect(req.request.method).toBe('POST');
+      req.flush(mockResponse);
+    });
   });
 });
