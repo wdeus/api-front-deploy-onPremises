5e09c1f8d07ad07a47d9b2efe8f94567bd0de349
Matheus Fiebig <macrubig@gmail.com>
2024-09-21 22:21:38 -0300

    PX-19 - criar servico de integracao com backend

--- api/src/app/services/dashboard.service.ts
+++ api/src/app/services/dashboard.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { DashboardRequest } from '../models/dashboard-request.model'
-import { HttpClient } from  '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+export type GraphicData = [number, string];
+export type CardData = [number];
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +13,13 @@ import { HttpClient } from  '@angular/common/http';
 export class DashboardService {
   constructor(private httpClient: HttpClient) { }
 
-  getCardData(request: DashboardRequest) : Observable<number> {
-    return of(1);
+  getCardData(request: DashboardRequest): Observable<CardData> {
+    const url = `${environment.apiUrl}visualizacao/card`;
+    return this.httpClient.post<CardData>(url, request);
+  }
+
+  getGraphicData(request: DashboardRequest): Observable<GraphicData[]> {
+    const url = `${environment.apiUrl}visualizacao/grafico`;
+    return this.httpClient.post<GraphicData[]>(url, request);
   }
 }
